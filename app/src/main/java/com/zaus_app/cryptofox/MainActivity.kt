97410f51603bfd3e50be31acf3fa6d5adad407ad
KrokodilGenadiy <alex.usevich@gmail.com>
2022-11-22 21:46:58 +0300

    set up navigation

--- app/src/main/java/com/zaus_app/cryptofox/MainActivity.kt
+++ app/src/main/java/com/zaus_app/cryptofox/MainActivity.kt
@@ -2,10 +2,56 @@ package com.zaus_app.cryptofox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.zaus_app.cryptofox.databinding.ActivityMainBinding
+import com.zaus_app.cryptofox.view.HomeFragment
+import com.zaus_app.cryptofox.view.NewsFragment
+import com.zaus_app.cryptofox.view.SearchFragment
+import com.zaus_app.cryptofox.view.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun checkFragmentExistence(tag: String): Fragment? = supportFragmentManager.findFragmentByTag(tag)
+
+    private fun changeFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment, tag)
+            .commit()
+    }
+
+    private fun initNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it) {
+                R.id.home -> {
+                    val tag = "home"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: HomeFragment(), tag)
+                }
+                R.id.search -> {
+                    val tag = "search"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: SearchFragment(), tag)
+                }
+                R.id.news -> {
+                    val tag = "news"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: NewsFragment(), tag)
+                }
+                R.id.settings -> {
+                    val tag = "settings"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: SettingsFragment(), tag)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/zaus_app/cryptofox/view/SearchFragment.kt
@@ -0,0 +1,26 @@
+package com.zaus_app.cryptofox.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zaus_app.cryptofox.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
